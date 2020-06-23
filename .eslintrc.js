376dd6c4937fb814716b34b4044a404c09f2040a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-23 11:46:35 +0100

    react-router-dom, framer-motion and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }], // react using jsx.
+    'quotes': ['error', 'single'], // define single quotes as default to js files.
+    'jsx-quotes': ['error', 'prefer-single'], // define single quotes as default to jsx.
+  },
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => {
-  return (
-    <div id='home'>
-      <p>Home</p>
-    </div>
-  );
-}
+const Home = () => (
+  <div id='home'>
+    <p>Home</p>
+  </div>
+);
 
 export default Home;
