ce5242c320d3d6bcce9f9692d19ba8b4f1a11cf4
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-24 17:53:36 +0100

    title animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Home.css';
+import { motion } from 'framer-motion';
 import Card from '../components/Card';
+
+const titleVariants = {
+  initial: { y: '-100vh' },
+  animate: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 75,
+    },
+  },
+};
 
 const Home = () => (
   <div id='home'>
-    <h1>Home</h1>
+    <motion.h1
+      variants={titleVariants}
+      initial='initial'
+      animate='animate'
+    >
+      Home
+    </motion.h1>
     <div className='cards'>
       <Card
         url='/dark-theme'
