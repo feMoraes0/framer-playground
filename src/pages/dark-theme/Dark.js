3202f4d2a3fa141c9082fb6c6353ac35152334a6
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-24 17:47:04 +0100

    updated dark theme icons and text

--- src/pages/dark-theme/Dark.js
+++ src/pages/dark-theme/Dark.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Dark.css';
-import { RiArrowLeftLine, RiLightbulbFlashLine, RiLightbulbLine } from 'react-icons/ri';
+import { RiArrowLeftLine, RiMoonLine, RiSunLine } from 'react-icons/ri';
 import { motion, useCycle } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const backgroundVariants = {
   light: (width) => ({
-    clipPath: `circle(0px at ${width - 45}px 45px)`,
+    clipPath: `circle(22px at ${width - 45}px 45px)`,
     transition: { duration: 0.7, ease: 'easeOut' },
   }),
   dark: (width) => ({
@@ -53,7 +53,7 @@ const Dark = () => {
           variants={buttonVariants}
           animate={(lightOn) ? 'light' : 'dark'}
         >
-          {(lightOn) ? <RiLightbulbLine size={25} /> : <RiLightbulbFlashLine size={25} />}
+          {(lightOn) ? <RiSunLine size={25} /> : <RiMoonLine size={25} />}
         </motion.button>
         <motion.h1
           variants={textVariants}
@@ -65,7 +65,7 @@ const Dark = () => {
           variants={textVariants}
           animate={(lightOn) ? 'light' : 'dark'}
         >
-          Press the lamp to switch between light and dark theme.
+          Press the button at the top right to switch between light and dark theme.
         </motion.h3>
       </div>
     </>
