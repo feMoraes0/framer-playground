85c2130de8661d28816924e49b3a4c34caa565ba
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-25 23:19:15 +0100

    <background-words>: back-side text animation

--- src/pages/background-words/Background.js
+++ src/pages/background-words/Background.js
@@ -2,12 +2,41 @@ import React from 'react';
 import './Background.css';
 import { motion } from 'framer-motion';
 
+const backSizeVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.4,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const backSizeTextVariants = {
+  initial: { y: '-100vh' },
+  animate: {
+    y: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
+
 const Background = () => (
   <div id='background'>
-    <motion.div className='back-side'>
-      <motion.h1>BACK 01</motion.h1>
-      <motion.h1>BACK 02</motion.h1>
-      <motion.h1>BACK 03</motion.h1>
+    <motion.div
+      variants={backSizeVariants}
+      initial='initial'
+      animate='animate'
+      className='back-side'
+    >
+      <motion.h1 variants={backSizeTextVariants}>BACK 01</motion.h1>
+      <motion.h1 variants={backSizeTextVariants}>BACK 02</motion.h1>
+      <motion.h1 variants={backSizeTextVariants}>BACK 03</motion.h1>
     </motion.div>
     <motion.div className='front-side'>
       <motion.h1>frontal</motion.h1>
